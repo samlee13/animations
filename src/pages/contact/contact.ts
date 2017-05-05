fb275c64d59e77ef192cee6ef6551dc005697e16
Sam Rhodes <samantha_lee_509@yahoo.com>
2017-05-04 20:01:33 -0500

    cleaning

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-import { fade } from '../../shared/animations';
 
 @Component({
     selector: 'pages-contact',
     templateUrl: 'contact.html',
     styleUrls: ['./contact.css'],
-    animations: [ fade ]
 })
 
-export class ConPage {
-    state: string = 'visible';
-
-    fade(state){
-    console.log("it works3");
-    this.state = (this.state === 'visible' ? 'invisible' : 'visible');
-  }
-}
+export class ConPage {}
