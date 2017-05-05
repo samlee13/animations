34ac8ddca25dcd11d7f1fb3631c25f213215c9da
Sam Rhodes <samantha_lee_509@yahoo.com>
2017-05-05 13:38:22 -0500

    adding comments for animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//importing animations from other file.
 import { grow, flyInOut, fade, flyIn, fadeOut } from '../shared/animations';
 import { HomePage , ConPage } from '../pages/pages';
 import { state } from '@angular/animations';
@@ -8,18 +9,25 @@ import { state } from '@angular/animations';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  //you can just put all the animations in this array, but just importing them is much prettier.
   animations: [ flyInOut , fade, flyIn, fadeOut ]
 })
 export class AppComponent {
-state: string = 'small';
+//this line of code is crucial. nothing will work without this.
+//the string could be anything, it just has to be equal to a string.
+state: string = 'sam';
 
     flyInOut(state){
     console.log("it works1");
+    //"if it's in, go out, otherwise, go in."
+    //? means go out
+    //: means otherwise, go in.
     this.state = (this.state === 'in' ? 'out' : 'in');
   }
   
     fade(state){
     console.log("it works3");
+    //the same principle applies to all the rest.
     this.state = (this.state === 'visible' ? 'invisible' : 'visible');
   }
 
